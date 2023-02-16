dcc4f53e0826d990428d5cff8f8e7285af6a0d5e
SHLee17 <azurite17@gmail.com>
2023-02-17 00:11:22 +0900

    23/02/17

--- Yahtzee/Assets/02. Script/Board/Board.cs
+++ Yahtzee/Assets/02. Script/Board/Board.cs
@@ -10,5 +10,31 @@ public class Board : MonoBehaviour
     [SerializeField]
     Sprite[] playerTurn;
 
+    [SerializeField]
+    List<Score> player1ScoreList;
+    [SerializeField]
+    List<Score> player2ScoreList;
 
+    public Dictionary<int,Dictionary<ScoreType, Score>> playerScore;
+
+    private void Start()
+    {
+        playerScore = new Dictionary<int,Dictionary<ScoreType, Score>>();
+
+        Dictionary<ScoreType, Score> score = new Dictionary<ScoreType, Score>();
+
+        foreach (Score item in player1ScoreList) 
+            score.Add(item.scoreType, item);
+        
+        playerScore.Add(1, score);
+
+        score.Clear();
+
+        foreach (Score item in player2ScoreList)
+            score.Add(item.scoreType, item);
+
+        playerScore.Add(2, score);
+
+        playerScore[1][ScoreType.Deuces].SetText();
+    }
 }
